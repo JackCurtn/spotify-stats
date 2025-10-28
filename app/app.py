d28b235b89890e9514bab811c1d3d122557b87fa
Jack <j.curtin98@gmail.com>
2025-10-28 23:15:30 +0100

    fixed app.py port, updated README

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import spotify_api
+import os
 
 
 app = Flask(__name__)
@@ -33,4 +34,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
